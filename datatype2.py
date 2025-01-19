f9bb9d4b4c1dea5f8d57370cc3560e7277b94ebb
Adema <ademaaman0711@gmail.com>
2025-01-19 23:56:28 +0500

    Добавлены новые файлы

---
+++ datatype2.py
@@ -0,0 +1,56 @@
+x = 20.5
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+y = 1j
+
+#display x:
+print(y)
+
+#display the data type of x:
+print(type(y)) 
+z = ["apple", "banana", "cherry"]
+
+#display x:
+print(z)
+
+#display the data type of x:
+print(type(z)) 
+s= ("apple", "banana", "cherry")
+
+#display x:
+print(s)
+
+#display the data type of x:
+print(type(s)) 
+a = range(6)
+
+#display x:
+print(a)
+
+#display the data type of x:
+print(type(a)) 
+b= {"name" : "John", "age" : 36}
+
+#display x:
+print(b)
+
+#display the data type of x:
+print(type(b)) 
+c = {"apple", "banana", "cherry"}
+
+#display x:
+print(c)
+
+#display the data type of x:
+print(type(c)) 
+d = True
+
+#display x:
+print(d)
+
+#display the data type of x:
+print(type(d)) 
